41fadc2e481ab7f6b39ac811be0e3c83d6a092e1
Liu Chong <liuchong@about.me>
2017-03-22 13:50:27 +0800

    Update access token

--- api_controller.gemspec
+++ api_controller.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
   s.name = "api_controller"
-  s.version = "0.1.0"
+  s.version = "0.1.1"
   s.date = "2017-03-08"
   s.summary = "api controller"
   s.authors = "Liu Chong <liuchong@about.me>"
--- lib/api_controller.rb
+++ lib/api_controller.rb
@@ -4,7 +4,10 @@ class APIController < ActionController::API
   protected
     def record_not_found(e)
       if e.message[/User/]
-        render json: { error: "TOKEN-ERROR" }
+        return render json: { error: "TOKEN-ERROR" }
+      end
+      if e.message[/Channel/]
+        return render json: { error: "ACCESS-TOKEN-ERROR", msg: "访问令牌错误" }
       end
     end
 end
